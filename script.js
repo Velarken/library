9fa6b3795d18e145d6a275c979541744c2008105
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-05-27 10:12:53 -0400

    Created all initial files

---
+++ script.js
@@ -0,0 +1,10 @@
+const myLibrary = [];
+
+function Book() {
+    // constructor
+    // use crypto.randomUUID() to generate unique ID's for each book
+}
+
+function addBookToLibrary() {
+    // take parameters, create a book, then push to myLibrary array
+}
